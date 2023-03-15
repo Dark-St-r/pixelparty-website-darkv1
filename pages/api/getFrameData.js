11250adf90de1a72a7413bc59afa955f773e06a8
chloe <107150192+doulos819@users.noreply.github.com>
2023-03-15 19:44:26 -0400

    Update getFrameData.js
    Signed-off-by: chloe <107150192+doulos819@users.noreply.github.com>

--- pages/api/getFrameData.js
+++ pages/api/getFrameData.js
@@ -24,38 +24,41 @@ export default async (req, res) => {
 
   const account = await getAnonAccount();
   const frames = await Promise.all(Array(10).fill().map(async (_, i) => {
-    const start = i * 30; // Changed from 60 to 30
-    const end = start + 30; // Changed from 60 to 30
+    const start = i * 30;
+    const end = start + 30;
     return await loadFrames(account, start, end);
   }));
-  let i = 0;
+
   frames.forEach(element => {
-    element.metadata.forEach(m => {
-      metadata.push(m);
-    });
+    if (element.metadata) {
+      element.metadata.forEach(m => {
+        metadata.push(m);
+      });
+    }
 
-    element.data.forEach(d => {
-      try {
-        let check = JSON.parse(StorageManager.decompressB64(d));
+    if (element.data) {
+      element.data.forEach(d => {
+        try {
+          let check = JSON.parse(StorageManager.decompressB64(d));
 
-        if (check.length != 400) {
-          throw "wrong pixel size";
+          if (check.length != 400) {
+            throw "wrong pixel size";
+          }
+          check.forEach((element) => {
+            if (element > 236) {
+              throw "color undefined";
+            }
+            if (element < 0) {
+              throw "color undefined";
+            }
+          });
+          data.push(check);
+        } catch (e) {
+          console.log(e);
+          data.push(JSON.parse(StorageManager.decompressB64("WzIzNCzfBN8E3wTfBN8E3wTfBN8E1AQxNjDJBN8s3wTOTN9U3wTWTN9U30zeBN9EzUzcVMwc30zRNNBY1GjfUNkE2CDfUN9Q31DfUN9Q31D/AUDXVNxQ2EjYVN9M/wD4/wCY3wTfTN8E31TfTN8E2VTfLN8E3wTfBN8E3wTJBF0=")));
         }
-        check.forEach((element) => {
-          if (element > 236) {
-            throw "color undefined";
-          }
-          if (element < 0) {
-            throw "color undefined";
-          }
-        });
-        data.push(check);
-      } catch (e) {
-        console.log(e);
-        data.push(JSON.parse(StorageManager.decompressB64("WzIzNCzfBN8E3wTfBN8E3wTfBN8E1AQxNjDJBN8s3wTOTN9U3wTWTN9U30zeBN9EzUzcVMwc30zRNNBY1GjfUNkE2CDfUN9Q31DfUN9Q31D/AUDXVNxQ2EjYVN9M/wD4/wCY3wTfTN8E31TfTN8E2VTfLN8E3wTfBN8E3wTJBF0=")));
-      }
-    });
-
+      });
+    }
   });
 
   res.statusCode = 200;
